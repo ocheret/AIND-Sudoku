63ee4e60353ebeecba73fe5a13cdec1aaa67d8e2
Chuck Ocheret <ocheret@panix.com>
2017-01-26 18:22:42 -0500

    Implemented basics, cross, grid_values, display.

--- solution.py
+++ solution.py
@@ -1,5 +1,53 @@
 assignments = []
 
+def cross(major, minor):
+    """Cross product of elements in A and elements in B.
+    Args:
+        major(string) - string of labels for major axis
+        minor(string) - string of labels for minor axis
+    Returns:
+        list of strings for every combination of major and minor labels
+    """
+    return [a + b for a in major for b in minor]
+
+def chunks(l, n):
+    """Partition l into n-sized chunks."""
+    return [l[i:i+n] for i in range(0, len(l), n)]
+
+# Column labels
+column_labels =  '123456789'
+
+# Row labels
+row_labels = 'ABCDEFGHI'
+
+# Valid digits
+digits = '123456789'
+
+# Lables for all boxes in the puzzle
+boxes = cross(row_labels, column_labels)
+
+# All row units
+row_units = [cross(row, column_labels) for row in row_labels]
+
+# All column units
+column_units = [cross(row_labels, col) for col in column_labels]
+
+# All box units
+box_units = [cross(a, b) for a in chunks(row_labels, 3) for b in chunks(column_labels, 3)]
+
+# All diagonal units
+diagonal_units = [[a+b for a,b in zip(row_labels, column_labels)],
+                  [a+b for a,b in zip(row_labels, column_labels[::-1])]]
+
+# All units
+all_units = row_units + column_units + box_units + diagonal_units
+
+# Mapping from boxes to units
+units = dict((box, [unit for unit in all_units if box in unit]) for box in boxes)
+
+# Mapping from boxes to peers
+peers = dict((box, set(sum(units[box], [])) - set([box])) for box in boxes)
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -22,10 +70,6 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
-
 def grid_values(grid):
     """
     Convert grid into a dict of {square: char} with '123456789' for empties.
@@ -36,7 +80,7 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    return dict(zip(boxes, [(lambda x: x if x in digits else digits)(c) for c in grid ]))
 
 def display(values):
     """
@@ -44,7 +88,14 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    # Figure out the max width of each column
+    widths = list(map(max, [[1 + len(values[b])for b in c] for c in column_units]))
+    horizontal_line = '+'.join(map(lambda width: '-' * (width + 1), map(sum, chunks(widths, 3))))
+    horizontal_line = horizontal_line[1:-1]
+    for r in row_labels:
+        print(''.join([values[r + str(c + 1)].center(widths[c]) + ('| ' if c in [2, 5] else '') for c in range(9)]))
+        if r in ['C', 'F']:
+            print(horizontal_line)
 
 def eliminate(values):
     pass
@@ -67,6 +118,9 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    board = grid_values(grid)
+    display(board)
+    pass
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
