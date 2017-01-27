f3ad10130f0c3bfd11a56d5d7a42ac392325e992
Chuck Ocheret <ocheret@panix.com>
2017-01-27 16:48:38 -0500

    Implemented all functionality successfully.

--- solution.py
+++ solution.py
@@ -97,17 +97,84 @@ def display(values):
         if r in ['C', 'F']:
             print(horizontal_line)
 
+def naked_twins(values):
+    for unit in all_units:
+        pairs = [values[box] for box in unit if len(values[box]) == 2]  # Find all 2 characer values
+        twins = [pair for pair in set(pairs) if pairs.count(pair) == 2] # Find all twins
+        for twin in twins:
+            for box in unit:
+                if values[box] != twin:
+                    assign_value(values, box, values[box].replace(twin[0], '').replace(twin[1], ''))
+    return values
+
 def eliminate(values):
-    pass
+    # Write a function that will take as an input, the sudoku in dictionary form,
+    # run through all the boxes, applying the eliminate technique,
+    # and return the resulting sudoku in dictionary form.
+    for s, v in values.items():
+        if (len(v) == 1):
+            for p in peers[s]:
+                assign_value(values, p, values[p].replace(v, ''))
+    return values
 
 def only_choice(values):
-    pass
+    # Write a function that will take as an input, the sudoku in dictionary form,
+    # run through all the units, applying the only choice techniqnue,
+    # and return the resulting sudoku in dictionary form.
+    digits = list(str(x) for x in range(10))
+    for u in all_units:
+        for d in digits:
+            occurrences = [b for b in u if d in values[b]]
+            if len(occurrences) == 1:
+                assign_value(values, occurrences[0], d)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+        values = eliminate(values)
+        values = naked_twins(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    if all(len(values[x]) == 1 for x in boxes):
+        return values
+
+    # Chose one of the unfilled square s with the fewest possibilities
+    size_list = [(b, len(v)) for b, v in values.items() if len(v) > 1]
+    fewest_box = ''
+    fewest_nums = 10
+    for box, nums in size_list:
+        if nums < fewest_nums:
+            fewest_box = box
+            fewest_nums = nums
+
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for c in values[fewest_box]:
+        values_copy = values.copy()
+        assign_value(values_copy, fewest_box, c)
+        result = search(values_copy)
+        if result is not False:
+            return result
+
+    return False
 
 def solve(grid):
     """
@@ -118,9 +185,10 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
-    board = grid_values(grid)
-    display(board)
-    pass
+    values = grid_values(grid)
+    display(values)
+    search(values)
+    return values
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
